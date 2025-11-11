c82603b166802bc27a7d3ebd734cf514b5f146e8
Rubén Vázquez Amos <rubenvazquezamos@gmail.com>
2025-11-11 16:23:45 +0000

    restructuring code

--- main.m
+++ main.m
@@ -4,25 +4,33 @@
 % This script optimises a geometry and later generates it in COMSOL
 % to compare diffusion coefficients
 % 
+% This code uses Loosely coupled components, so variables are often restated.
 
+%% OPTIMISATION
 run("optimiseGeometry.m")
 clear all
 load("optgeo.mat")
-% 
-geometry = struct();
-geometry.neck.l_n = optgeo.neck.lengths;
-geometry.neck.w_n = optgeo.neck.widths;
-geometry.cavity.w_c = optgeo.cavity.widths;
-geometry.cavity.l_c = optgeo.cavity.lengths;
-geometry.slit.hh = optgeo.slit.widths;
-geometry.slit.a_y = optgeo.slit.lengths;
-geometry.L = optgeo.panel_thickness;
+
+geometry = structureGeometry(optgeo);
 
 N = 5;
-e = 1e-3;
+stock_thickness = 12e-3;
 
-QRM_geometry = geo_generator(geometry,N,e); %generate geometry
+QRM_geometry = geo_generator(geometry,N,stock_thickness);
 QRM_geometry.resetHist %compact model history
 QRM_geometry.geom('geom1').export('optgeo.mphbin') %save .mphbin geometry file
 
 run("scatteredPressure_main");
+
+function geometry = structureGeometry(optgeo)
+    
+    geometry = struct();
+    geometry.neck.l_n = optgeo.neck.lengths;
+    geometry.neck.w_n = optgeo.neck.widths;
+    geometry.cavity.w_c = optgeo.cavity.widths;
+    geometry.cavity.l_c = optgeo.cavity.lengths;
+    geometry.slit.hh = optgeo.slit.widths;
+    geometry.slit.a_y = optgeo.slit.lengths;
+    geometry.L = optgeo.panel_thickness;
+
+end
